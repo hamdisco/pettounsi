1a2669ea6cc39b6a8bd24cba6a78d257bd286b5b
hamdisco <105083183+hamdisco@users.noreply.github.com>
2026-04-02 20:09:27 +0100

    strange fix

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,7 +3,7 @@ import UIKit
 import FirebaseCore
 import FirebaseAppCheck
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
